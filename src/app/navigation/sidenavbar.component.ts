f6aeda82fe7e7e4cd965b7330ad5fb54f687a7ec
Abby Baker <aeb0046@mix.wvu.edu>
2022-03-06 22:23:25 -0500

    adding components

---
+++ src/app/navigation/sidenavbar.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component ({
+    selector: "fm-sidenavbar",
+    templateUrl: "sidenavbar.component.html",
+    styleUrls: ["sidenavbar.component.css", ]
+})
+
+export class SideNavBarComponent {
+
+}
