e3e2b6ab9ddf47beef7adeb632f19ecc82a2d9c6
ABby Baker <aeb0046@mix.wvu.edu>
2022-03-08 19:44:15 -0500

    removed http dependency

--- src/app/browse/products.service.ts
+++ src/app/browse/products.service.ts
@@ -1,19 +1,18 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ProductItemModel } from "./product-item.model";
+import { AngularFireDatabase } from "@angular/fire/compat/database"
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductsService {
-    private baseUrl:string = "https://apple-music-6e6d8-default-rtdb.firebaseio.com/";
-    private productsEndpoint:string = "products.json";
-    
-    constructor(private http:HttpClient) {
+    constructor(private db: AngularFireDatabase) {
 
     }
 
     public getProducts() {
-        return this.http.get<ProductItemModel []>(this.baseUrl + this.productsEndpoint)
+        return this.db.list<ProductItemModel>("products").valueChanges();
     }
+
 }
